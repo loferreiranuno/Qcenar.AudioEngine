4944fcbf6f4728e0eabd45b196e37ba0b7ac5cca
loferreira <loferreira.nuno@gmail.com>
2023-02-04 12:06:04 +0100

    a

--- src/app.py
+++ src/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/receive_url", methods=['POST'])
+def receive_url():
+    url = request.form.get('url')
+    if url:
+        # Perform operations with the received URL
+        return "URL received: {}".format(url)
+    else:
+        return "No URL provided"
+
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0')
